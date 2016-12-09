2594e9daf3798bcff0c12c2fd1f178d1b2dd5c8d
imnawaz <mohdshameem36@gmail.com>
2016-12-09 12:46:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<html><h1>TEST</h1>
- <script> 
-   $.getJSON('//freegeoip.net/json/?callback=?', function(data) {
-  console.log(JSON.stringify(data, null, 2));
-});
-  </script>
-  </html>
-$(id)
-%windir%
 <?php
 $ip = getenv('HTTP_CLIENT_IP')?:
 getenv('HTTP_X_FORWARDED_FOR')?:
